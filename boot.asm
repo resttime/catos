e88d680593dc72c48966eda94282a7f854506150
Daniel Jiang <djiang11@illinois.edu>
2019-01-03 23:08:42 -0600

    Print a small message

--- boot.asm
+++ boot.asm
@@ -1,8 +1,26 @@
-; Infinite loop
-loop:
-    jmp loop
+;; https://en.wikipedia.org/wiki/INT_10H
+;; Using the 0x10h BIOS interrupt
+    mov ah, 0x0e
+    mov al, 'A'
+    int 0x10
+    mov al, 'y'
+    int 0x10
+    mov al, 'y'
+    int 0x10
+    mov al, ' '
+    int 0x10
+    mov al, 'l'
+    int 0x10
+    mov al, 'm'
+    int 0x10
+    mov al, 'a'
+    int 0x10
+    mov al, 'o'
+    int 0x10
 
-; Fill with 510
-times 510-($-$$) db 0
-; Magic number
-dw 0xaa55
+;; Infinite loop, $ means the current line
+    jmp $
+
+;; Pad up with zeros and magic number to 512
+    times 510-($-$$) db 0
+    dw 0xaa55
