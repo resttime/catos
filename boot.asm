9da2de59cb826a621db72504beda3c7ebedc065c
Daniel Jiang <djiang11@illinois.edu>
2019-01-07 23:48:41 -0600

    Fix the jumps with printing
    Apparently the PUSHA and POPA instructions are removed in x86_64

--- boot.asm
+++ boot.asm
@@ -4,28 +4,34 @@
 ;; https://en.wikipedia.org/wiki/INT_10H
 ;; Using the 0x10h BIOS interrupt
 
-    mov bx, string
+    mov bx, hello
     call print_string
+
+    mov bx, bye
+    call print_string
+
 ;; Infinite loop, $ means the current line
     jmp $
 
 ;; Print using 'teletype output' and stack
 print_string:
-    pusha                       ; Push all regs
-    mov ah, 0x0e
     mov al, [bx]
-    or al, al
-    jz print_end
-    int 0x10                    ; Print interrupt
+    cmp al, 0
+    je ($+4)                    ; Return if NULL
+    call print_char
     inc bx
     jmp print_string
-print_end:
-    popa                        ; Pop all regs and return
+    ret
+print_char:
+    mov ah, 0x0e
+    int 0x10
     ret
 
-string:
-    db 'Hello World!',0
-    
+hello:
+    db 'Hello World!',10,13,0
+bye:
+    db 'Bye Bye!',10,13,0
+
 ;; Pad up with zeros and magic number to 512
     times 510-($-$$) db 0
     dw 0xaa55
