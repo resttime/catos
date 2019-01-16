f078288b07fdf3c6060986332a5fade88e198947
Daniel Jiang <djiang11@illinois.edu>
2019-01-15 23:40:00 -0600

    Add in asm function for printing hex!

--- boot.asm
+++ boot.asm
@@ -4,16 +4,56 @@
 ;; https://en.wikipedia.org/wiki/INT_10H
 ;; Using the 0x10h BIOS interrupt
 
+;; Put stack someplace that won't overwrite things
+    mov bp, 0x8000
+    mov sp, bp
+;; Print
     mov bx, hello
     call print_string
-
+;; Print
     mov bx, bye
     call print_string
-
+;; Print
+    mov dx, 0x1fb6
+    call print_hex
 ;; Infinite loop, $ means the current line
     jmp $
 
-;; Print using 'teletype output' and stack
+;; Print hex stored in dx
+print_hex:
+    push 0
+
+    mov bx, dx
+    shr bx, 0
+    and bx, 0xF
+    add bx, hex
+    mov ah, [bx]
+
+    mov bx, dx
+    shr bx, 4
+    and bx, 0xF
+    add bx, hex
+    mov al, [bx]
+    push ax
+
+    mov bx, dx
+    shr bx, 8
+    and bx, 0xF
+    add bx, hex
+    mov ah, [bx]
+
+    mov bx, dx
+    shr bx, 12
+    and bx, 0xF
+    add bx, hex
+    mov al, [bx]
+    push ax
+
+    mov bx, sp
+    call print_string
+    ret
+
+;; Prints the NULL terminated string pointed by bx
 print_string:
     mov al, [bx]
     cmp al, 0
@@ -22,16 +62,22 @@ print_string:
     inc bx
     jmp print_string
     ret
+;; Prints the character in al
 print_char:
     mov ah, 0x0e
     int 0x10
     ret
 
+;;; Strings
+;; 10: '\n' and 13: '\r'
 hello:
     db 'Hello World!',10,13,0
 bye:
     db 'Bye Bye!',10,13,0
+hex:
+    db '0123456789abcdef'
 
+;;; End!
 ;; Pad up with zeros and magic number to 512
     times 510-($-$$) db 0
     dw 0xaa55
