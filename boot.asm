6262183bd39095f52e3bee697bb93fd171424d89
resttime <resttime@users.noreply.github.com>
2020-10-25 00:31:23 -0500

    Add disk_load routine and stuff

--- boot.asm
+++ boot.asm
@@ -4,7 +4,8 @@
 ;; https://en.wikipedia.org/wiki/INT_10H
 ;; Using the 0x10h BIOS interrupt
 
-;; Put stack someplace that won't overwrite things
+;; Place the stack where it won't ovewrite things
+;; 0x8000 is EBDA (Extended BIOS Data Area)
     mov bp, 0x8000
     mov sp, bp
 ;; Print
@@ -16,7 +17,41 @@
 ;; Print
     mov dx, 0x1fb6
     call print_hex
+
 ;; Infinite loop, $ means the current line
+    jmp $
+
+;; load DH sectors to ES:BX from drive DL
+disk_load:
+    push dx
+
+    ; Bios read function
+    mov ah, 0x02
+    ; Read dh sectors
+    mov al, dh
+    ; cylinder 0
+    mov ch, 0x00
+    ; head 0
+    mov dh, 0x00
+    ; read second sector (after boot sector)
+    mov cl, 0x02
+    ; BIOS interrupt
+    int 0x13
+
+    ; Jump if error
+    jc disk_error
+
+    ; restore dx
+    pop dx
+
+    ; jump to error if sectors read != expected
+    cmp dh, al
+    jne disk_error
+    ret
+
+disk_error:
+    mov bx, DISK_ERROR_MSG
+    call print_string
     jmp $
 
 ;; Print hex stored in dx
@@ -30,10 +65,17 @@ print_hex:
     push ax
 
     ; Push the characters onto the stack
-    mov bx, dx
-    and bx, 0xF
-    add bx, hex
-    mov ah, [bx]
+    ; 1. Grab the value of the hexchar
+    ; 2. Use the value to offset hex address to pick hexchar
+    ; 3. Place the hexchar into register
+    ; 4. Push register into stack (16bit push so
+    ;    set both al,ah before pushing ax)
+    ; 5. print stack
+    mov bx, dx                  ; set bx to the hex
+    shr bx, 0                   ; shift right
+    and bx, 0xF                 ; zero out for offset
+    add bx, hex                 ; hexchar location
+    mov ah, [bx]                ; move hexchar into register
 
     mov bx, dx
     shr bx, 4
@@ -58,6 +100,9 @@ print_hex:
     ; Call print string on the stack
     mov bx, sp
     call print_string
+
+    ; Print newline
+    call print_newline
 
     ; Restore the registers and return
     pop ax
@@ -101,6 +146,10 @@ bye:
     db 'Bye Bye!',10,13,0
 hex:
     db '0123456789abcdef'
+secret:
+    db 'X'
+
+DISK_ERROR_MSG db "Disk read error!",0
 
 ;;; End!
 ;; Pad up with zeros and magic number to 512
